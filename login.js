46ac787ca8da069ce33035853a040f35a2856b0a
PetroShulzhenko <64135837+PetroShulzhenko@users.noreply.github.com>
2020-05-21 09:33:34 +0300

    Add files via upload

--- login.js
+++ login.js
@@ -12,8 +12,32 @@ function postLogin(e) {
     xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
 
     xhr.onload = function(){
-        if( this.responseText == true) {
+        if( (this.status == 200) && (JSON.parse(this.responseText) == true) ) {
             window.location.href = "../test/src/pages/Home.vue";
+        }
+        else {
+            alert("Что-то пошло не так..");
+        }
+    }
+
+    xhr.send(params);
+}
+
+document.getElementById("forgot-password").addEventListener('click',forgotPassword);
+
+function forgotPassword(e) {
+    e.preventDefault();
+
+    var login = document.getElementById("login").value;
+    var params = "login=" + login; 
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'SERVER FILE HERE !!!!!!', true);
+    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+
+    xhr.onload = function(){
+        if( (this.status == 200) && (JSON.parse(this.responseText) == true) ) {
+            alert("На ваш Email отправлено письмо :)");
         }
         else {
             alert("Что-то пошло не так..");
