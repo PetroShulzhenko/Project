97f5220e334734014f5cb1510df81ac4e89c53c7
PetroShulzhenko <64135837+PetroShulzhenko@users.noreply.github.com>
2020-05-21 09:59:19 +0300

    Add files via upload

--- login.js
+++ login.js
@@ -23,26 +23,3 @@ function postLogin(e) {
     xhr.send(params);
 }
 
-document.getElementById("forgot-password").addEventListener('click',forgotPassword);
-
-function forgotPassword(e) {
-    e.preventDefault();
-
-    var login = document.getElementById("login").value;
-    var params = "login=" + login; 
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'SERVER FILE HERE !!!!!!', true);
-    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-
-    xhr.onload = function(){
-        if( (this.status == 200) && (JSON.parse(this.responseText) == true) ) {
-            alert("На ваш Email отправлено письмо :)");
-        }
-        else {
-            alert("Что-то пошло не так..");
-        }
-    }
-
-    xhr.send(params);
-}
